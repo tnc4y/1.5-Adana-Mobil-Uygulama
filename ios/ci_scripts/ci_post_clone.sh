ada0bf956b8be38d45a9d094598feb3f0d715101
tnc4y <tuncayyilmazvip@gmail.com>
2026-05-14 23:26:59 +0300

    Update code signing settings to automatic and specify development team ID; enhance CI post-clone script for Xcode setup

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-# 1. Flutter'ı bulut makinesine indir (stable kanal)
+# 1. Flutter'ı indir
 git clone https://github.com/flutter/flutter.git -b stable $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
 
-# 2. Bağımlılıkları yükle
+# 2. Ana dizine git ve paketleri çek
 cd ../..
 flutter pub get
 
-# 3. CocoaPods kurulumu
+# 3. İŞTE BURAYA EKLE: Xcode için gerekli dosyaları önceden oluştur
+flutter build ios --config-only --release
+
+# 4. iOS klasörüne dön ve Pod'ları kur
 cd ios
 pod install
 
